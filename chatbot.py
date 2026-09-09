23bab2fc470a34f87a7e72172377468bd4506515
Vivek Sarma <viveksarmapasarlapati@gmail.com>
2026-09-09 11:28:21 +0530

    Add conversation history memory

--- chatbot.py
+++ chatbot.py
@@ -1,25 +1,22 @@
 import ollama
-
 print("Chatbot has started.")
-print("Type 'exit', 'quit', 'bye', 'goodbye', or 'end' to stop.")
-
+print("Type any exit commands to end the conversation with the bot.")
 exit_commands = ["exit", "quit", "bye", "goodbye", "end"]
-
 while True:
     user_input = input("You: ")
-
     if user_input.lower() in exit_commands:
         print("Exiting the chatbot. Goodbye!")
         break
-
+    messages.append({
+        "role": "user",
+        "content": user_input
+    })
     response = ollama.chat(
         model="qwen3:4b",
-        messages=[
-            {
-                "role": "user",
-                "content": user_input
-            }
-        ]
+        messages=messages
     )
-
+    messages.append({
+    "role": "assistant",
+    "content": response.message.content 
+    })
     print("Chatbot:", response.message.content)
